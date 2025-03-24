1429b9b6999ccc0ac11dfc7e1ee63446c4066e46
AlishanArshad <aa9641434@gmail.com>
2025-03-24 17:45:02 +0500

    LinkedList Initialization

---
+++ LinkedList/LinkedListDemo.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        Node a = new Node();
+        a.data = 5;
+        a.next = null;
+        System.out.println(a.data);
+        System.out.println(a.next);
+    }
+}
